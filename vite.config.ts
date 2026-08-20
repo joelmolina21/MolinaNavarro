48459e22e6526d19d396e8334260881c017b3079
joelmolina21 <144848244+joelmolina21@users.noreply.github.com>
2026-08-21 01:15:59 +0200

    he añadido base a vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/MolinaNavarro/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
